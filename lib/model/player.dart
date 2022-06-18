70a3ba4d9915b3bc73b4be6e16e8ca51465dc9d7
Apridev <apriantofajar11@gmail.com>
2022-06-18 20:46:22 +0900

    player model

---
+++ lib/model/player.dart
@@ -0,0 +1,12 @@
+class Player{
+  String id, name, position, imageUrl;
+  DateTime createdAt;
+
+  Player({
+    this.position, 
+    this.id,
+    this.name,
+    this.imageUrl,
+    this.createdAt
+  });
+}
